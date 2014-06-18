88c94b9e4112620582ad4b3f905b0c4376976777
unknown <rzavala@RZAVALA01.noam.cemexnet.com>
2014-06-18 18:57:23 -0500

    submission 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MyFirst Function will set a value of a matrix
+## get value of the matrix , store the inverse , and get the inverse
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  #Set the value of the matrix and m to Null to indicate this is the first time set 
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  #prints the value of the matrix inputed
+  get <- function() x
+  #Sets to m the value of the inverse of the matrix if calculated 
+  #using other function cacheSolve
+  setinverse <- function(inverse) m <<- inverse
+  #when i run setinverse i assign to m  the value of the inverse (not null)  
+  #Get/prints the value of the inverse  
+  getinverse <- function() m
+#special list to store functions and values
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## cacheSolve gives you the inverse of a matrix if not stored in memory
+#calculates it , if stored in cache retrieves it from there
+
+cacheSolve <- function(x, ...) {
+  #assign to local m the value of the m stored in global environment 
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  #call function to store new m value in global env..
+  x$setinverse(m)
+  m        
 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cachemean <- function(x, ...) {
+  
 }
